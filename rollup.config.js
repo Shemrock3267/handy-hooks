336aa80730be49ce3428a7c54a6c37d54bd7aa2f
Shemrock3267 <dev.oliinyk@gmail.com>
2023-05-12 19:53:31 +0300

    fixed rollup dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,17 @@
 import typescript from "rollup-plugin-ts";
 import terser from "@rollup/plugin-terser";
 
-const packageName = require('./package.json').name;
-
 export default {
   input: "src/index.ts",
   plugins: [typescript(), terser()],
   output: [
     {
-      file: `./dist/${packageName}.esm.js`,
+      file: `./dist/esm/index.esm.js`,
       format: "esm",
       exports: "auto"
     },
     {
-      file: `./dist/${packageName}.js`,
+      file: `./dist/cjs/index.js`,
       format: "cjs",
       exports: "auto"
     },
